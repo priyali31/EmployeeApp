75abc515588d66da28388b317fbf8d44e8e2162c
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-13 21:10:47 +0530

    added EmpApp.java

--- src/com/employeewage/EmpApp.java
+++ src/com/employeewage/EmpApp.java
@@ -1,25 +1,52 @@
-import java.util.Random;
 import java.io.*;
+import java.util.Random;
 class EmpApp
 {
 
+	private int PerHour,workHour,DayHour,payment;
+	public static Random rand=new Random();
+
+	EmpApp(int PerHour,int workHour,int DayHour)
+	{
+		this.PerHour=PerHour;
+		this.workHour=workHour;
+		this.DayHour=DayHour;
+	}
+
+	public int PerHour()
+	{
+		return this.PerHour;
+	}
+
+	public void setPerHour(int input)
+	{
+		this.PerHour=input;
+	}
+
 	public static boolean isPresent()
 	{
-		Random rand=new Random();
-		int random=rand.nextInt(100)%2;
+		int random=rand.nextInt(50)%2;
 		return random==1?true:false; 
+	}
+
+	public int calculatePayment()
+	{
+		this.payment=this.PerHour*this.workHour;
+		return this.payment; 
 	}
 
 	public static void main(String[] args)
 		{
+		EmpApp appObj=new EmpApp(20,rand.nextInt(20),8);
+		System.out.println(appObj.calculatePayment());
 		System.out.println("Computation");
 			if(isPresent())
 			{
-			  System.out.println("Present");
+				System.out.println("Employee is Present");
 			}
 			else
 			{
-			  System.out.println("Absent");
+				System.out.println("Employee is Absent");
 			}
 		}
 }
