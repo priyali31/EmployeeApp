9beba2c5a9c102211ec94ec66c96dba73bbd80f5
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-14 00:11:24 +0530

    100 working hours

--- src/com/employeewage/test/test.java
+++ src/com/employeewage/test/test.java
@@ -1,4 +1,5 @@
 package com.employeewage.test;
+import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.InputMismatchException;
@@ -6,65 +7,60 @@ import com.employeewage.model.Emp;
 import com.employeewage.service.EmpApp;
 
 class test
-{
+	{
 
 	public static int testTest()
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("1.Today's Payment is");
-		System.out.println("2.Monthly Payment is");
-		System.out.println("3.Employee was");
-		System.out.println("4.present or absent");
-		System.out.println("5.Exit");
-		System.out.println("Enter Choice : ");
-		int choice=sc.nextInt();
-		System.out.println();
+		System.out.println("2.Monthly Payment");
+		System.out.println("3.Conditional Payment is");
+		System.out.println("4.Employee was");
+		System.out.println("5.Present or Absent");
+		System.out.println("6.Exit");
+		System.out.println("Choice : ");
+		int choice = sc.nextInt();
 		return choice;	
 	}
 
 	public static void main(String[] args)
 	{
 		try{
-			System.out.println("Computation");
-			Random rand=new Random();
-			EmployeeApp app=new EmployeeApp();
-			Emp emp=new Emp(20,rand.nextInt(20),8);
+			System.out.println("Computation Program");
+			Random rand= new Random();
+			EmployeeApp app= new EmployeeApp();
+			Emp emp = new Emp(20,rand.nextInt(20),8);
 
 			while(true)
-			{
+				{
 				int option=testTest();
 				switch(option)
-				{
+					{
 					case 1:
-					System.out.println("Today's Payment is  " + app.calculatePayment(emp));
+					System.out.println("Today's Payment is " +app.calculatePayment(emp));
 					break;
 					case 2:
-					System.out.println("Monthly Payment is" + app.monthlyCalculatePayment(emp));
+					System.out.println("Monthly Pay is " +app.monthlyCalculatePayment(emp));
 					break;
 					case 3:
-					System.out.println("Employee was " + emp.getEmployeeName());
+					System.out.println("Conditional Pay is "+app.hundredCalculatePayment(emp));
 					break;
 					case 4:
-					if(app.isPresent(emp))
-					{
-					System.out.println("Present");
-					}
-					else
-					{
-					System.out.println("Absent");
-					}
+					System.out.println("Employee was " +app.getEmployeeName(emp));
 					break;
 					case 5:
+					System.out.println(app.isPresent(emp)?"Present":"Absent");
+					break;
+					case 6:
 					System.exit(0);
 					break;
+					}
 
 				}
-
-			}
-		    }catch(InputMismatchException e)
+			}catch(InputMismatchException e)
 				{
-				System.out.println("Wrong input");
-				}
-	}
-
+				System.out.println("Wrong Number");
+		                }		
+		}
+	}	
 }
