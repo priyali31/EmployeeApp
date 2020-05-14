e7228bb8c37699e2e15efe0c14da95decae775b2
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-14 23:25:40 +0530

    Array list of company

--- src/com/employeewage/service/EmpApp.java
+++ src/com/employeewage/service/EmpApp.java
@@ -1,59 +1,54 @@
 package com.employeewage.service;
-import java.util.InputMismatchException;
 import com.employeewage.model.Emp;
 import java.util.Random;
+import java.util.*;
+import java.io.*;
 
-public class EmpApp
-{
+public class EmpApp implements EmpService{
 
+	private List<Company> companyList;
+
+	@Override
 	public boolean isPresent(Emp emp)
 	{
-		if(emp.getWorkHour() < 1)
-		{
-			return false;
-		}
-		return true;
+		int random=new Random().nextInt(100)%2;
+		return random==1?true:false;
 	}
 
-	public int calculatePayment(Emp emp)
+	public int randomWorkHours(String companyName){
+		int dayHour = this.findCompany(companyName).getMaxHoursPerMonth();
+		return new Random().nextInt(dayHour)%2;
+	}
+
+	public void addEmployees(String companyName,int ratePerHour,int WorkingDays,int maxHoursPerMonth)
 	{
-		emp.setPayment(emp.getPerHour()*emp.getWorkHour());
-		return emp.getPay();
+		Company cp=new Company(companyName,ratePerHour,WorkingDays,maxHoursPerMonth);
+		cp.setEmpList(wrongEmployee(companyName,workingDays));
+		System.out.println("Employee in company");
 	}
 
-	public String getEmployeeName(Emp emp)
+	public ArrayList<Employee> wrongEmployee(String companyName, int WorkingDays){
+		ArrayList<Employee> empList = new ArrayList<>();
+		Emp ep = null;
+		for(int i=0;i<10;i++)
+			{
+			ep = new Employee("name of employee"+i, companyName,new ArrayList<Integer>());
+			for(int j=0;j<workingDays;j++)
+			{
+			ep.getWages().add(new Random().nextInt(10));
+			}
+			empList.add(ep);
+		}
+		return empList;
+	}
+
+	public Company findCompany(String companyName)
 	{
-		if((emp.getWorkHour()/20)<emp.getDayHour())
+		for (Company company : companyList) 
 		{
-			emp.setEmployeeName("Part time");
+			if(company.getCompanyName())
+				return company;
 		}
-		else
-		{
-			emp.setEmployeeName("Full time");
-		}
-		return emp.getEmployeeName();		
+		return null;
 	}
-
-	public int monthlyCalculatePayment(Emp emp)
-		{
-		Random rand=new Random();
-		for(int i=0;i<20;i++)
-		{
-			emp.setWorkHour(emp.getWorkHour()+rand.nextInt(20));
-		}
-		emp.setPayment(emp.getPerHour()*emp.getWorkHour());
-		return emp.getPayment();
-	}
-
-	public int hundredCalculatePayment(Emp emp)
-		{
-		Random rand=new Random();
-		while(emp.getWorkHour()<=100)
-		{
-			emp.setWorkHour(emp.getWorkHour()+rand.nextInt(20));
-		}
-		emp.setPay(emp.getPerHour() * emp.getWorkHour());
-		return emp.getPayment();
-	}
-
 }
